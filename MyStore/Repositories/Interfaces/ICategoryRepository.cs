1e9b191132aad53d7d0aa4b2438c2814fa144e14
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-08-22 19:29:18 +0330

    Category repository added

---
+++ MyStore/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using MyStore.Models;
+
+namespace MyStore.Repositories.Interfaces
+{
+	public interface ICategoryRepository
+	{
+		IEnumerable<Category> GetAllCategories();
+		Category GetCategoryById(int id);
+		void AddCategory(Category category);
+		void DeleteCategory(int id);
+		void UpdateCategory(Category category);
+
+	}
+}
---
+++ MyStore/Repositories/Services/CategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using MyStore.Data;
+using MyStore.Models;
+using MyStore.Repositories.Interfaces;
+
+namespace MyStore.Repositories.Services
+{
+	public class CategoryRepository : ICategoryRepository
+	{
+		private readonly MyStrorDbContext myStoreDbContext;
+
+		public CategoryRepository(MyStrorDbContext myStoreDbContext)
+        {
+			this.myStoreDbContext = myStoreDbContext;
+		}
+
+		private void Save()
+		{
+			myStoreDbContext.SaveChanges();
+		}
+
+		public IEnumerable<Category> GetAllCategories()
+		{
+			return myStoreDbContext.Categories.ToList();
+		}
+
+		public Category GetCategoryById(int id)
+		{
+			return myStoreDbContext.Categories.ToList().Find(x => x.CategoryId == id );
+		}
+
+		public void AddCategory(Category category)
+		{
+			myStoreDbContext.Categories.Add(category);
+		}
+
+		public void DeleteCategory(int id)
+		{
+			myStoreDbContext.Categories.Remove(GetCategoryById(id));
+		}
+
+		public void UpdateCategory(Category category)
+		{
+			var existedCategory = GetCategoryById(category.CategoryId);
+			if (existedCategory != null)
+			{
+				existedCategory = category;
+				myStoreDbContext.Categories.ToList()[existedCategory.CategoryId] = existedCategory;
+				Save();
+			}
+		}
+    }
+}
