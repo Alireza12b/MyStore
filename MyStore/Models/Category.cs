cefdfbdfe06b96e042d782922906b3de6bed73b3
Alireza Bagheri <alirezabagheri12b@gmail.com>
2023-08-22 18:17:31 +0330

    Add project files.

---
+++ MyStore/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyStore.Models
+{
+	public class Category
+	{
+		public int CategoryId { get; set; }
+		public string Name { get; set; }
+
+		[ForeignKey("ProductId")]
+		public int ProductId { get; set; }
+		public virtual ICollection<Product> Products { get; set; }
+	}
+}
---
+++ MyStore/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace MyStore.Models
+{
+	public class Product
+	{
+		public int ProductId { get; set; }
+		public string Name { get; set; }
+		public int Price { get; set; }
+		public string ManufactureDate { get; set; }
+
+		public virtual Category Category { get; set; }
+	}
+}
